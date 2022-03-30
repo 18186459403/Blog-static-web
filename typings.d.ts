d58b1c555ce881dec85343a81a0f702f16fa3e06
csc <1909270710@qq.com>
2022-03-30 22:00:56 +0800

    test

--- typings.d.ts
+++ typings.d.ts
@@ -1,6 +1,7 @@
 declare module '*.css';
 declare module '*.less';
 declare module '*.png';
+declare var API_PREFIX: string;
 declare module '*.svg' {
   export function ReactComponent(
     props: React.SVGProps<SVGSVGElement>,
