028bb17cfaeb0b29036c63bbbdf49d5a6ccb879a
csc <1909270710@qq.com>
2022-05-03 22:46:40 +0800

    simple layout

--- config/router.ts
+++ config/router.ts
@@ -1,1 +1,29 @@
-export default [{ path: '/', component: '@/pages/index' }];
+export default [
+  { path: '/', redirect: '/login' },
+  {
+    path: '/login',
+    component: '@/pages/Login',
+  },
+  {
+    path: '/blog',
+    component: '@/layouts',
+    routes: [
+      {
+        path: '/blog/atricle',
+        component: '@/pages/Atricle',
+      },
+      {
+        path: '/blog/atricle/list',
+        component: '@/pages/AtricleList',
+      },
+      {
+        path: '/blog/home',
+        component: '@/pages/Home',
+      },
+      {
+        path: '/blog/user',
+        component: '@/pages/User',
+      },
+    ],
+  },
+];
